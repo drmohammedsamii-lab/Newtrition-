676ae38efe2e571dadac8aa718fdbd156c381c23
drmohammedsamii-lab <dr.mohammedsamii@gmail.com>
2026-08-16 21:44:06 +0300

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -1,252 +1,290 @@
-// jscs:disable requireUseStrict
+/*!
+ * http-errors
+ * Copyright(c) 2014 Jonathan Ong
+ * Copyright(c) 2016 Douglas Christopher Wilson
+ * MIT Licensed
+ */
 
-var test = require('tape');
+'use strict'
 
-var functionBind = require('../implementation');
-var getCurrentContext = function () { return this; };
+/**
+ * Module dependencies.
+ * @private
+ */
 
-test('functionBind is a function', function (t) {
-    t.equal(typeof functionBind, 'function');
-    t.end();
-});
+var deprecate = require('depd')('http-errors')
+var setPrototypeOf = require('setprototypeof')
+var statuses = require('statuses')
+var inherits = require('inherits')
+var toIdentifier = require('toidentifier')
 
-test('non-functions', function (t) {
-    var nonFunctions = [true, false, [], {}, 42, 'foo', NaN, /a/g];
-    t.plan(nonFunctions.length);
-    for (var i = 0; i < nonFunctions.length; ++i) {
-        try { functionBind.call(nonFunctions[i]); } catch (ex) {
-            t.ok(ex instanceof TypeError, 'throws when given ' + String(nonFunctions[i]));
-        }
+/**
+ * Module exports.
+ * @public
+ */
+
+module.exports = createError
+module.exports.HttpError = createHttpErrorConstructor()
+module.exports.isHttpError = createIsHttpErrorFunction(module.exports.HttpError)
+
+// Populate exports for all constructors
+populateConstructorExports(module.exports, statuses.codes, module.exports.HttpError)
+
+/**
+ * Get the code class of a status code.
+ * @private
+ */
+
+function codeClass (status) {
+  return Number(String(status).charAt(0) + '00')
+}
+
+/**
+ * Create a new HTTP Error.
+ *
+ * @returns {Error}
+ * @public
+ */
+
+function createError () {
+  // so much arity going on ~_~
+  var err
+  var msg
+  var status = 500
+  var props = {}
+  for (var i = 0; i < arguments.length; i++) {
+    var arg = arguments[i]
+    var type = typeof arg
+    if (type === 'object' && arg instanceof Error) {
+      err = arg
+      status = err.status || err.statusCode || status
+    } else if (type === 'number' && i === 0) {
+      status = arg
+    } else if (type === 'string') {
+      msg = arg
+    } else if (type === 'object') {
+      props = arg
+    } else {
+      throw new TypeError('argument #' + (i + 1) + ' unsupported type ' + type)
     }
-    t.end();
-});
+  }
 
-test('without a context', function (t) {
-    t.test('binds properly', function (st) {
-        var args, context;
-        var namespace = {
-            func: functionBind.call(function () {
-                args = Array.prototype.slice.call(arguments);
-                context = this;
-            })
-        };
-        namespace.func(1, 2, 3);
-        st.deepEqual(args, [1, 2, 3]);
-        st.equal(context, getCurrentContext.call());
-        st.end();
-    });
+  if (typeof status === 'number' && (status < 400 || status >= 600)) {
+    deprecate('non-error status code; use only 4xx or 5xx status codes')
+  }
 
-    t.test('binds properly, and still supplies bound arguments', function (st) {
-        var args, context;
-        var namespace = {
-            func: functionBind.call(function () {
-                args = Array.prototype.slice.call(arguments);
-                context = this;
-            }, undefined, 1, 2, 3)
-        };
-        namespace.func(4, 5, 6);
-        st.deepEqual(args, [1, 2, 3, 4, 5, 6]);
-        st.equal(context, getCurrentContext.call());
-        st.end();
-    });
+  if (typeof status !== 'number' ||
+    (!statuses.message[status] && (status < 400 || status >= 600))) {
+    status = 500
+  }
 
-    t.test('returns properly', function (st) {
-        var args;
-        var namespace = {
-            func: functionBind.call(function () {
-                args = Array.prototype.slice.call(arguments);
-                return this;
-            }, null)
-        };
-        var context = namespace.func(1, 2, 3);
-        st.equal(context, getCurrentContext.call(), 'returned context is namespaced context');
-        st.deepEqual(args, [1, 2, 3], 'passed arguments are correct');
-        st.end();
-    });
+  // constructor
+  var HttpError = createError[status] || createError[codeClass(status)]
 
-    t.test('returns properly with bound arguments', function (st) {
-        var args;
-        var namespace = {
-            func: functionBind.call(function () {
-                args = Array.prototype.slice.call(arguments);
-                return this;
-            }, null, 1, 2, 3)
-        };
-        var context = namespace.func(4, 5, 6);
-        st.equal(context, getCurrentContext.call(), 'returned context is namespaced context');
-        st.deepEqual(args, [1, 2, 3, 4, 5, 6], 'passed arguments are correct');
-        st.end();
-    });
+  if (!err) {
+    // create error
+    err = HttpError
+      ? new HttpError(msg)
+      : new Error(msg || statuses.message[status])
+    Error.captureStackTrace(err, createError)
+  }
 
-    t.test('called as a constructor', function (st) {
-        var thunkify = function (value) {
-            return function () { return value; };
-        };
-        st.test('returns object value', function (sst) {
-            var expectedReturnValue = [1, 2, 3];
-            var Constructor = functionBind.call(thunkify(expectedReturnValue), null);
-            var result = new Constructor();
-            sst.equal(result, expectedReturnValue);
-            sst.end();
-        });
+  if (!HttpError || !(err instanceof HttpError) || err.status !== status) {
+    // add properties to generic error
+    err.expose = status < 500
+    err.status = err.statusCode = status
+  }
 
-        st.test('does not return primitive value', function (sst) {
-            var Constructor = functionBind.call(thunkify(42), null);
-            var result = new Constructor();
-            sst.notEqual(result, 42);
-            sst.end();
-        });
+  for (var key in props) {
+    if (key !== 'status' && key !== 'statusCode') {
+      err[key] = props[key]
+    }
+  }
 
-        st.test('object from bound constructor is instance of original and bound constructor', function (sst) {
-            var A = function (x) {
-                this.name = x || 'A';
-            };
-            var B = functionBind.call(A, null, 'B');
+  return err
+}
 
-            var result = new B();
-            sst.ok(result instanceof B, 'result is instance of bound constructor');
-            sst.ok(result instanceof A, 'result is instance of original constructor');
-            sst.end();
-        });
+/**
+ * Create HTTP error abstract base class.
+ * @private
+ */
 
-        st.end();
-    });
+function createHttpErrorConstructor () {
+  function HttpError () {
+    throw new TypeError('cannot construct abstract class')
+  }
 
-    t.end();
-});
+  inherits(HttpError, Error)
 
-test('with a context', function (t) {
-    t.test('with no bound arguments', function (st) {
-        var args, context;
-        var boundContext = {};
-        var namespace = {
-            func: functionBind.call(function () {
-                args = Array.prototype.slice.call(arguments);
-                context = this;
-            }, boundContext)
-        };
-        namespace.func(1, 2, 3);
-        st.equal(context, boundContext, 'binds a context properly');
-        st.deepEqual(args, [1, 2, 3], 'supplies passed arguments');
-        st.end();
-    });
+  return HttpError
+}
 
-    t.test('with bound arguments', function (st) {
-        var args, context;
-        var boundContext = {};
-        var namespace = {
-            func: functionBind.call(function () {
-                args = Array.prototype.slice.call(arguments);
-                context = this;
-            }, boundContext, 1, 2, 3)
-        };
-        namespace.func(4, 5, 6);
-        st.equal(context, boundContext, 'binds a context properly');
-        st.deepEqual(args, [1, 2, 3, 4, 5, 6], 'supplies bound and passed arguments');
-        st.end();
-    });
+/**
+ * Create a constructor for a client error.
+ * @private
+ */
 
-    t.test('returns properly', function (st) {
-        var boundContext = {};
-        var args;
-        var namespace = {
-            func: functionBind.call(function () {
-                args = Array.prototype.slice.call(arguments);
-                return this;
-            }, boundContext)
-        };
-        var context = namespace.func(1, 2, 3);
-        st.equal(context, boundContext, 'returned context is bound context');
-        st.notEqual(context, getCurrentContext.call(), 'returned context is not lexical context');
-        st.deepEqual(args, [1, 2, 3], 'passed arguments are correct');
-        st.end();
-    });
+function createClientErrorConstructor (HttpError, name, code) {
+  var className = toClassName(name)
 
-    t.test('returns properly with bound arguments', function (st) {
-        var boundContext = {};
-        var args;
-        var namespace = {
-            func: functionBind.call(function () {
-                args = Array.prototype.slice.call(arguments);
-                return this;
-            }, boundContext, 1, 2, 3)
-        };
-        var context = namespace.func(4, 5, 6);
-        st.equal(context, boundContext, 'returned context is bound context');
-        st.notEqual(context, getCurrentContext.call(), 'returned context is not lexical context');
-        st.deepEqual(args, [1, 2, 3, 4, 5, 6], 'passed arguments are correct');
-        st.end();
-    });
+  function ClientError (message) {
+    // create the error object
+    var msg = message != null ? message : statuses.message[code]
+    var err = new Error(msg)
 
-    t.test('passes the correct arguments when called as a constructor', function (st) {
-        var expected = { name: 'Correct' };
-        var namespace = {
-            Func: functionBind.call(function (arg) {
-                return arg;
-            }, { name: 'Incorrect' })
-        };
-        var returned = new namespace.Func(expected);
-        st.equal(returned, expected, 'returns the right arg when called as a constructor');
-        st.end();
-    });
+    // capture a stack trace to the construction point
+    Error.captureStackTrace(err, ClientError)
 
-    t.test('has the new instance\'s context when called as a constructor', function (st) {
-        var actualContext;
-        var expectedContext = { foo: 'bar' };
-        var namespace = {
-            Func: functionBind.call(function () {
-                actualContext = this;
-            }, expectedContext)
-        };
-        var result = new namespace.Func();
-        st.equal(result instanceof namespace.Func, true);
-        st.notEqual(actualContext, expectedContext);
-        st.end();
-    });
+    // adjust the [[Prototype]]
+    setPrototypeOf(err, ClientError.prototype)
 
-    t.end();
-});
+    // redefine the error message
+    Object.defineProperty(err, 'message', {
+      enumerable: true,
+      configurable: true,
+      value: msg,
+      writable: true
+    })
 
-test('bound function length', function (t) {
-    t.test('sets a correct length without thisArg', function (st) {
-        var subject = functionBind.call(function (a, b, c) { return a + b + c; });
-        st.equal(subject.length, 3);
-        st.equal(subject(1, 2, 3), 6);
-        st.end();
-    });
+    // redefine the error name
+    Object.defineProperty(err, 'name', {
+      enumerable: false,
+      configurable: true,
+      value: className,
+      writable: true
+    })
 
-    t.test('sets a correct length with thisArg', function (st) {
-        var subject = functionBind.call(function (a, b, c) { return a + b + c; }, {});
-        st.equal(subject.length, 3);
-        st.equal(subject(1, 2, 3), 6);
-        st.end();
-    });
+    return err
+  }
 
-    t.test('sets a correct length without thisArg and first argument', function (st) {
-        var subject = functionBind.call(function (a, b, c) { return a + b + c; }, undefined, 1);
-        st.equal(subject.length, 2);
-        st.equal(subject(2, 3), 6);
-        st.end();
-    });
+  inherits(ClientError, HttpError)
+  nameFunc(ClientError, className)
 
-    t.test('sets a correct length with thisArg and first argument', function (st) {
-        var subject = functionBind.call(function (a, b, c) { return a + b + c; }, {}, 1);
-        st.equal(subject.length, 2);
-        st.equal(subject(2, 3), 6);
-        st.end();
-    });
+  ClientError.prototype.status = code
+  ClientError.prototype.statusCode = code
+  ClientError.prototype.expose = true
 
-    t.test('sets a correct length without thisArg and too many arguments', function (st) {
-        var subject = functionBind.call(function (a, b, c) { return a + b + c; }, undefined, 1, 2, 3, 4);
-        st.equal(subject.length, 0);
-        st.equal(subject(), 6);
-        st.end();
-    });
+  return ClientError
+}
 
-    t.test('sets a correct length with thisArg and too many arguments', function (st) {
-        var subject = functionBind.call(function (a, b, c) { return a + b + c; }, {}, 1, 2, 3, 4);
-        st.equal(subject.length, 0);
-        st.equal(subject(), 6);
-        st.end();
-    });
-});
+/**
+ * Create function to test is a value is a HttpError.
+ * @private
+ */
+
+function createIsHttpErrorFunction (HttpError) {
+  return function isHttpError (val) {
+    if (!val || typeof val !== 'object') {
+      return false
+    }
+
+    if (val instanceof HttpError) {
+      return true
+    }
+
+    return val instanceof Error &&
+      typeof val.expose === 'boolean' &&
+      typeof val.statusCode === 'number' && val.status === val.statusCode
+  }
+}
+
+/**
+ * Create a constructor for a server error.
+ * @private
+ */
+
+function createServerErrorConstructor (HttpError, name, code) {
+  var className = toClassName(name)
+
+  function ServerError (message) {
+    // create the error object
+    var msg = message != null ? message : statuses.message[code]
+    var err = new Error(msg)
+
+    // capture a stack trace to the construction point
+    Error.captureStackTrace(err, ServerError)
+
+    // adjust the [[Prototype]]
+    setPrototypeOf(err, ServerError.prototype)
+
+    // redefine the error message
+    Object.defineProperty(err, 'message', {
+      enumerable: true,
+      configurable: true,
+      value: msg,
+      writable: true
+    })
+
+    // redefine the error name
+    Object.defineProperty(err, 'name', {
+      enumerable: false,
+      configurable: true,
+      value: className,
+      writable: true
+    })
+
+    return err
+  }
+
+  inherits(ServerError, HttpError)
+  nameFunc(ServerError, className)
+
+  ServerError.prototype.status = code
+  ServerError.prototype.statusCode = code
+  ServerError.prototype.expose = false
+
+  return ServerError
+}
+
+/**
+ * Set the name of a function, if possible.
+ * @private
+ */
+
+function nameFunc (func, name) {
+  var desc = Object.getOwnPropertyDescriptor(func, 'name')
+
+  if (desc && desc.configurable) {
+    desc.value = name
+    Object.defineProperty(func, 'name', desc)
+  }
+}
+
+/**
+ * Populate the exports object with constructors for every error class.
+ * @private
+ */
+
+function populateConstructorExports (exports, codes, HttpError) {
+  codes.forEach(function forEachCode (code) {
+    var CodeError
+    var name = toIdentifier(statuses.message[code])
+
+    switch (codeClass(code)) {
+      case 400:
+        CodeError = createClientErrorConstructor(HttpError, name, code)
+        break
+      case 500:
+        CodeError = createServerErrorConstructor(HttpError, name, code)
+        break
+    }
+
+    if (CodeError) {
+      // export the constructor
+      exports[code] = CodeError
+      exports[name] = CodeError
+    }
+  })
+}
+
+/**
+ * Get a class name from a name identifier.
+ *
+ * @param {string} name
+ * @returns {string}
+ * @private
+ */
+
+function toClassName (name) {
+  return name.slice(-5) === 'Error' ? name : name + 'Error'
+}
